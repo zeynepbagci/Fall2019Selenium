4148549e5f55a586e85f8be3740940b206b7ec14
zeynepbagci <zenepozcan@gmail.comq>
2020-03-01 13:46:04 -0500

    creating basic navigation

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -8,18 +8,22 @@ import org.openqa.selenium.safari.SafariDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //to start selenium script we need:
         //setup webdriver and create webdriver object
 
         WebDriverManager.chromedriver().setup();
 
+        //we will run tests in ChromeDriver();
         WebDriver driver = new ChromeDriver();
         //because  I'll use chrome
         //in selenium, everything starts from WebDriver Interface
         //ChromeDriver is a class obviously
+        //ChromeDriver extends RemoteWebDriver--> implements WebDriver
         driver.get("http://google.com");//to open a website
+        Thread.sleep(3000);//for demo, wait 3 seconds
+        driver.close();//to close driver
 
     }
 }
