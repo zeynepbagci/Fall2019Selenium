9192d05496c6abfade53cc4bc5a1c9445a9605e1
zeynepbagci <zenepozcan@gmail.comq>
2020-03-03 20:33:24 -0500

    DriverFactory

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -29,6 +29,17 @@ public class FindElementsPractice {
 
         Thread.sleep(2000);
 
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+
+        WebElement message = driver.findElement(By.className("subheader")) ;
+
+        String actual = message.getText();//to get the tezt <h3>Text</h3>
+
+        if(expected.equals(actual)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED");
+        }
         driver.quit();//to close everything
 
     }
