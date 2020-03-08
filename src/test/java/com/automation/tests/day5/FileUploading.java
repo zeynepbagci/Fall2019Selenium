80a620bdf7c8dbcde53cb01afaa26ce781677e06
zeynepbagci <zenepozcan@gmail.comq>
2020-03-08 06:24:17 -0400

    day4

--- src/test/java/com/automation/tests/day5/FileUploading.java
+++ src/test/java/com/automation/tests/day5/FileUploading.java
@@ -10,14 +10,28 @@ public class FileUploading {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().version("79").setup();
         WebDriver driver = new ChromeDriver();
+
         driver.get("http://practice.cybertekschool.com/upload");
         BrowserUtils.wait(5);
+
         WebElement upload = driver.findElement(By.id("file-upload"));
+
         //https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
         //I am gonna upload pom.xml file
         String filePath = System.getProperty("user.dir")+"/pom.xml";
-        upload.sendKeys(filePath);
+        //it work only for my computer because only have this file
+        //and my computer username is different than yours
+        String file2Path = "/Users/studio2/Downloads/image (2).png";
+//        doesn't work, should be on your computer
+//        String cloudFile = "https://cybertek-appium.s3.amazonaws.com/etsy.apk";
+
+        System.out.println(filePath);//print path
+
+        upload.sendKeys(file2Path);
+
         driver.findElement(By.id("file-submit")).click();//click to upload
+
+
         BrowserUtils.wait(5);
         driver.quit();
     }
