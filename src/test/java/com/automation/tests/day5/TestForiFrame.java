3255f8144c2e065b5c53721b036b888bb4f5c201
zeynepbagci <zenepozcan@gmail.comq>
2020-03-08 06:23:34 -0400

    day4

--- src/test/java/com/automation/tests/day5/TestForiFrame.java
+++ src/test/java/com/automation/tests/day5/TestForiFrame.java
@@ -11,14 +11,10 @@ public class TestForiFrame {
 
     public static void main(String[] args) {
 
-        //iframe is like another body inside of the body
-
-
         WebDriverManager.chromedriver().version("79").setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cybertekschool.com/iframe");
         BrowserUtils.wait(4);
-
 
         //before looking for that element, we need to jump to that frame
         //you can specify: name, id, index or webelement of the frame
@@ -26,13 +22,23 @@ public class TestForiFrame {
         driver.switchTo().frame("mce_0_ifr");
 
         //now, this content will be visible
-        //yani frame icindeki content in id sini girerek bunu bastirdik
         WebElement textInput = driver.findElement(By.id("tinymce"));
+
         System.out.println(textInput.getText());
 
+        BrowserUtils.wait(4);
+
+        textInput.clear();//to delete text
+        textInput.sendKeys("Hello, World!");
+
+        BrowserUtils.wait(4);
 
         //exit from the frame
         driver.switchTo().defaultContent();
+
+        WebElement heading = driver.findElement(By.tagName("h3"));
+        System.out.println(heading.getText());
+
         driver.quit();
     }
 }
