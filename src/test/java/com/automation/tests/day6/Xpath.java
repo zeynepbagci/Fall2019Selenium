2486110867e15a5ea29b6a0e952e2fa0d85f0c2d
zeynepbagci <zenepozcan@gmail.com>
2020-03-10 20:22:49 -0400

    day6

--- src/test/java/com/automation/tests/day6/Xpath.java
+++ src/test/java/com/automation/tests/day6/Xpath.java
@@ -13,19 +13,32 @@ public class Xpath {
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
         BrowserUtils.wait(3);
-
-        //value can be inside single quotes or double quotes
+        //value can be inside '' or ""
         //if you don't know the tag name, or want to skip tag name, use *
-        //for example //*[@onclick='button1()']
-        //* means any tag name
-        WebElement button1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
-        button1.click();
+        //for example //*[@onclick='button1()']  | * means any tag name
+
+        WebElement btn1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
+        btn1.click();
 
         WebElement result = driver.findElement(By.id("result"));
         System.out.println(result.getText());
 
+        //click on button #2
+        WebElement btn2 = driver.findElement(By.xpath("//button[text()='Button 2']"));
+        btn2.click();
+        System.out.println(result.getText());
 
+        WebElement btn3 = driver.findElement(By.xpath("//button[starts-with(@id,'button_')]"));
+        btn3.click();
+        System.out.println(result.getText());
 
+        WebElement btn4 = driver.findElement(By.xpath("//button[contains(@id,'_button')][1]"));
+        btn4.click();
+        System.out.println(result.getText());
+
+        WebElement btn5 = driver.findElement(By.xpath("//button[contains(text(),'5')]"));
+        btn5.click();
+        System.out.println(result.getText());
 
         BrowserUtils.wait(3);
         driver.quit();
