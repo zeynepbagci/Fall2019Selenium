2259da3ca4f4d6f8cb894f931be607d8388bad49
zeynepbagci <zenepozcan@gmail.com>
2020-03-15 17:53:47 -0400

    commit

--- src/test/java/com/automation/tests/vytrack/fleet/VehiclePageTest.java
+++ src/test/java/com/automation/tests/vytrack/fleet/VehiclePageTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -31,7 +32,9 @@ public class VehiclePageTest {
     private By passwordBy = By.id("prependedInput2");
     private By fleetBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Fleet')]");
     private By subtitleBy = By.className("oro-subtitle");
+    private By pageNumber = By.cssSelector("input[type='number']");
     private WebDriver driver;
+
     @Test
     public void verifyPageSubTitle(){
         //login
@@ -40,7 +43,13 @@ public class VehiclePageTest {
         //put more wait here as well, if didn't click
         BrowserUtils.wait(5);
         //click on fleet
-        driver.findElement(fleetBy).click();
+        //driver.findElement(fleetBy).click();
+
+        //Actions class is used for more advanced browser interactions
+        Actions actions = new Actions(driver);
+        //move to element instead of click
+        actions.moveToElement(driver.findElement(fleetBy)).perform();
+        //perform -  to execute command
         BrowserUtils.wait(2);
         //click on Vehicles
         driver.findElement(By.linkText("Vehicles")).click();
@@ -50,6 +59,32 @@ public class VehiclePageTest {
         //find subtitle element
         WebElement subTitleElement = driver.findElement(subtitleBy);
         System.out.println(subTitleElement.getText());
+
+        String expected = "All Cars";
+        String actual = subTitleElement.getText();
+
+        assertEquals(actual,expected);
+    }
+
+    /**
+     * /**
+     *  *
+     *  *     ################ TASK 5 minutes
+     *  *
+     *  *     Given user is on the vytrack landing page
+     *  *     When user logs on as a store manager
+     *  *     Then user navigates to Fleet --> Vehicles
+     *  *     And user verifies that page number is equals to "1"
+     *  */
+
+    @Test(description = "verify that page number is equals to :1 ")
+    public void verifyPafeNumber(){
+
+        String expected ="1";
+        String actual = driver.findElement(pageNumber).getAttribute("value");
+        assertEquals(expected,actual);
+
+
     }
     @BeforeMethod
     public void setup() {
@@ -57,6 +92,23 @@ public class VehiclePageTest {
         driver = new ChromeDriver();
         driver.get(URL);
         driver.manage().window().maximize();
+        //login
+        driver.findElement(usernameBy).sendKeys(username);
+        driver.findElement(passwordBy).sendKeys(password, Keys.ENTER);
+        //put more wait here as well, if didn't click
+        BrowserUtils.wait(5);
+        //click on fleet
+        //driver.findElement(fleetBy).click();
+
+        //Actions class is used for more advanced browser interactions
+        Actions actions = new Actions(driver);
+        //move to element instead of click
+        actions.moveToElement(driver.findElement(fleetBy)).perform();
+        //perform -  to execute command
+        BrowserUtils.wait(2);
+        //click on Vehicles
+        driver.findElement(By.linkText("Vehicles")).click();
+
     }
     @AfterMethod
     public void teardown() {
