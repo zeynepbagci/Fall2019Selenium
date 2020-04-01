570036a4d77d82d1ecdf024013799397e3460630
zeynepbagci <zenepozcan@gmail.com>
2020-04-01 07:29:16 -0400

    commit

--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -15,18 +15,25 @@ public class NewLoginTests extends AbstractTestBase {
      */
     @Test
     public void verifyPageTitle(){
-
+        //test-->ExtentTest object
+        //we must add to every test at the beginning
+        //test=report.createTest("Test name);
+        test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         Assert.assertEquals(Driver.getDriver().getTitle(),"Dashboard");
+        test.pass("Page title Dashboard was verified");
     }
     @Test
     public void verifyWarningMessage(){
+        test = report.createTest("Verify Warning message");
         LoginPage loginPage = new LoginPage();
         loginPage.login("wrong","wrong");
         Assert.assertEquals(loginPage.getWarningMessageText(),"Invalid user name or password.");
         //take a screenshot
         BrowserUtils.getScreenShot("warning_message");
+
+        test.pass("Warning message is displayed");
     }
 
 }
